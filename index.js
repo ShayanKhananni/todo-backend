10db55c7f5a40abd9d4edf1155a0f2408eef5326
Shayan Khanani <shani.khanani@gmail.com>
2024-09-16 23:01:32 +0500

    commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin:"https://todo-client-nof0naxta-shayans-projects-6c672329.vercel.app",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })

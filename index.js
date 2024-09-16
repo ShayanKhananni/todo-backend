47e8f62143711e4e9c2533969415626debb8dd54
Shayan Khanani <shani.khanani@gmail.com>
2024-09-17 00:44:22 +0500

    commit

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use((err, req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-  res.send(200).json({ message: "hello Wolrd" });
+  res.status(200).json({ message: "hello Wolrd" });
 });
 
 app.listen(3000, () => {

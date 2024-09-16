c14cde50a6925af455558ea5d693e61afa46e419
Shayan Khanani <shani.khanani@gmail.com>
2024-09-17 00:41:10 +0500

    commit

--- index.js
+++ index.js
@@ -38,6 +38,10 @@ app.use((err, req, res, next) => {
   });
 });
 
+app.get("/", (req, res) => {
+  res.send(200).json({ message: "hello Wolrd" });
+});
+
 app.listen(3000, () => {
   console.log("Server Started on port 3000");
 });

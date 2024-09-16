c7494475902b20b88899282e5229512a83565bf7
Shayan Khanani <shani.khanani@gmail.com>
2024-09-16 17:10:00 +0500

    Changes

--- index.js
+++ index.js
@@ -1,67 +1,57 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from "./routers/user-router.js"
+import userRouter from "./routers/user-router.js";
 import authRouter from "./routers/auth-router.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import todoRouter from "./routers/todo-router.js";
+import testRouter from "./routers/testrouter.js";
 
 dotenv.config();
 
 const app = express();
 
-
-app.use(cors({
-  origin: 'http://localhost:5173', 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
 
 app.use(express.json());
 
+app.use("/api/test", testRouter);
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/todo", todoRouter);
 
-app.use('/api/test',testRouter);
-app.use('/api/user',userRouter);
-app.use('/api/auth',authRouter);
-app.use('/api/todo',todoRouter);
-
-
-app.use((err,req,res,next)=>
-{
+app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
+  const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
-    success:false,
+    success: false,
     statusCode,
     message,
   });
 });
 
+app.listen(3000, () => {
+  console.log("Server Started on port 3000");
+});
 
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.listen(3000,()=>
-{
-  console.log("Server Started on port 3000")
-})
-
-
-mongoose.connect(process.env.MONGO)
-.then(()=>
-{
-  console.log("Connected to DB");
-})
-.catch((err)=>
-{
-  console.log(err);
-})
-
-app.get('/',(req,res)=>
-{
-  res.status(200).json({message:"Hello Wolrd"});
-})
-
-
-
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello Wolrd" });
+});
--- routers/testrouter.js
+++ routers/testrouter.js
@@ -1,8 +1,11 @@
 import express from "express";
 
 const router = express.Router();
+
 router.get('/testing',(req,res)=>
 {
   res.status(200).json({message:'end point is working'});
 })
 
+
+export default router;

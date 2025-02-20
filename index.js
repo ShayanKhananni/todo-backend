ab1c6b0517bda6474e70918ee5d1d0508ab4bfaa
Shayan Khanani <shani.khanani@gmail.com>
2025-02-20 10:42:31 +0500

    commit

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    // origin: "https://todo-client-six.vercel.app",
-    origin: "http://localhost:5173",
+    origin: "https://todo-frontend-38qe.vercel.app",
+    // origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ['Content-Type', 'Authorization'], 
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
@@ -27,7 +27,7 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/todo", todoRouter);
 
-app.use((err,req,res,next) => {
+app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
@@ -36,8 +36,6 @@ app.use((err,req,res,next) => {
     message,
   });
 });
-
-
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "hello Wolrd" });

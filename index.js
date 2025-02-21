15e367528d40e939b17da13b8b0aa867fc77e940
Shayan Khanani <shani.khanani@gmail.com>
2025-02-21 21:36:02 +0500

    commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: "https://todo-frontend-38qe.vercel.app",
+    origin: "https://my-todo-app-72.vercel.app",
     // origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
